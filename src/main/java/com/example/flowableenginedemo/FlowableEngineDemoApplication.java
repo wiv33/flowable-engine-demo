94d9066ee8c0e6ffa80525822ccd0fe69dc94108
PSK <wiv821253@gmail.com>
2023-10-09 21:49:13 +0900

    Initial commit

---
+++ src/main/java/com/example/flowableenginedemo/FlowableEngineDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.flowableenginedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowableEngineDemoApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(FlowableEngineDemoApplication.class, args);
+  }
+
+}
